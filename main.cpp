5bc87e10481037b721a621a8b4541b8802d15479
DevAndreww <andrea.sette2@marconibari.edu.it>
2024-10-24 07:51:14 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int N,i;
+    float numero, max;
+
+    cout << "Numero di valori da inserire?" << endl;
+
+    cin >> N;
+
+    for(i=1; i<=N; i=i+1)
+    {
+        cout << "Scrivi il numero." << endl,
+
+        cin >> numero;
+
+        if(i=1)
+        {
+            max=numero;
+        }
+        else
+        {
+            if(numero>max)
+            {
+                max = numero
+            }
+            else
+            {
+
+            }
+        }
+    }
+
+    cout << "Il numero maggiore è " << max << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
